7fc90da9e3e4297d72ee5856a6b815255d27278f
aga-maps <60704262+aga-maps@users.noreply.github.com>
2022-05-26 20:47:42 +0200

    searchTemperature function added

---
+++ client.js
@@ -0,0 +1,11 @@
+// used API:  https://goweather.herokuapp.com/weather/Gdansk
+
+function searchTemperature (){
+    const city = document.getElementById('cityInput').value;
+    document.getElementById('currentCity').innerHTML = city;
+    console.log(city)
+
+    fetch(`https://goweather.herokuapp.com/weather/${city}`)
+    .then(response => response.json())
+    .then(res => document.getElementById('temperature').innerHTML = res.temperature)
+}   
