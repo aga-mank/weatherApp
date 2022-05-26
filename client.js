61e4a167f5d62f3cb185b76d8493c9b2bca93a08
aga-maps <60704262+aga-maps@users.noreply.github.com>
2022-05-26 20:59:19 +0200

    styling and typo corrected

--- client.js
+++ client.js
@@ -2,10 +2,9 @@
 
 function searchTemperature (){
     const city = document.getElementById('cityInput').value;
-    document.getElementById('currentCity').innerHTML = city;
+    document.getElementById('currentCity').innerHTML = `City: ${city}`;
     console.log(city)
-
     fetch(`https://goweather.herokuapp.com/weather/${city}`)
     .then(response => response.json())
-    .then(res => document.getElementById('temperature').innerHTML = res.temperature)
+    .then(res => document.getElementById('temperature').innerHTML = `Temperature: ${res.temperature}`)
 }   
