5a8d7f0e244d01166a486db96d1403a6dd2e6ad4
aga-maps <60704262+aga-maps@users.noreply.github.com>
2022-05-29 10:37:00 +0200

    added icon, styling, changed api

--- client.js
+++ client.js
@@ -1,10 +1,63 @@
-// used API:  https://goweather.herokuapp.com/weather/Gdansk
+// used API:  https://weatherapi-com.p.rapidapi.com/current.json?q=Gdansk
 
-function searchTemperature (){
-    const city = document.getElementById('cityInput').value;
-    document.getElementById('currentCity').innerHTML = `City: ${city}`;
-    console.log(city)
-    fetch(`https://goweather.herokuapp.com/weather/${city}`)
-    .then(response => response.json())
-    .then(res => document.getElementById('temperature').innerHTML = `Temperature: ${res.temperature}`)
-}   
+//get input
+const input = document.getElementById('cityInput');
+
+//get output elements
+const currentCity = document.getElementById('currentCity');
+const temperature = document.getElementById('temperature');
+const description = document.getElementById('description');
+const pic = document.getElementById('image');
+
+//fetch options
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+        'X-RapidAPI-Key': 'cb30f6b1abmsh4f8e050a455d139p159f06jsn95c546a1f414'
+    }
+};
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function (event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("searchButton").click();
+    }
+});
+
+async function getData() {
+    //zbierz miasto z inputu
+
+    //pobierz dane z API
+    const weatherData = await fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${input.value}`, options)
+        .then(response => response.json());
+    console.log(weatherData);
+
+    //alternatywnie
+    //fetch('', options)
+    //.then(response => response.json)
+    //.then(data => {
+    //return data
+    //})
+
+    //pokaż miasto, państwo, temperaturę i opis
+    currentCity.innerHTML = `City: ${weatherData.location.name},  ${weatherData.location.country}`;
+    temperature.innerHTML = `Temperature: ${weatherData.current.temp_c}&#8451`;
+    description.innerHTML = `${weatherData.current.condition.text}`;
+
+
+    //kolorek
+    if (weatherData.current.temp_c > 15) {
+        temperature.style.color = 'rgb(171, 43, 43)', 5000
+    } else {
+        temperature.style.color = 'rgb(31, 59, 99)', 5000
+    }
+
+    //adding icon
+    console.log(weatherData.current.condition.icon);
+    pic.src = weatherData.current.condition.icon;
+}
